16e096b9d880a24dce9b336f5359b1329a687342
Corno Schraverus <corno@schraverus.com>
2026-06-09 13:29:30 +0200

    pdt: published version 0.1.2

--- typescript/lib/src/interface/resources.ts
+++ typescript/lib/src/interface/resources.ts
@@ -7,20 +7,17 @@ import * as d_stream_write_to_stderr from "./generated/liana/schemas/write_to_st
 import * as d_stream_write_to_stdout from "./generated/liana/schemas/write_to_stdout/data"
 import * as d_stream_get_instream_data from "./generated/liana/schemas/get_indata/data"
 
-export namespace stream {
+export namespace commands {
 
-    export namespace commands {
+    export type log = _pi.Command<null, d_stream_log.Parameters>
+    export type log_error = _pi.Command<null, d_stream_log_error.Parameters>
+    export type write_to_stderr = _pi.Command<null, d_stream_write_to_stderr.Parameters>
+    export type write_to_stdout = _pi.Command<null, d_stream_write_to_stdout.Parameters>
 
-        export type log = _pi.Command<null, d_stream_log.Parameters>
-        export type log_error = _pi.Command<null, d_stream_log_error.Parameters>
-        export type write_to_stderr = _pi.Command<null, d_stream_write_to_stderr.Parameters>
-        export type write_to_stdout = _pi.Command<null, d_stream_write_to_stdout.Parameters>
+}
 
-    }
+export namespace queries {
 
-    export namespace queries {
+    export type get_instream_data = _pi.Query<d_stream_get_instream_data.Result, null, d_stream_get_instream_data.Parameters>
 
-        export type get_instream_data = _pi.Query<d_stream_get_instream_data.Result, null, d_stream_get_instream_data.Parameters>
-
-    }
 }

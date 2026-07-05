beebd7e37a209bb4f561a8ea9fce5a06e5bea5cb
Corno Schraverus <corno@schraverus.com>
2026-07-05 10:22:26 +0200

    pdt: published version 0.1.16

--- typescript/lib/src/implementation/generated/liana/schemas/fountain_pen/transformers/prose.ts
+++ typescript/lib/src/implementation/generated/liana/schemas/fountain_pen/transformers/prose.ts
@@ -1,9 +1,9 @@
 
 import * as p_ from 'pareto-core/dist/implementation/transformer'
 
-import * as t_signatures from "../../../../../../interface/generated/liana/schemas/fountain_pen/signatures/transformers/fountain_pen"
+import * as t_signatures from "../../../../../../interface/generated/liana/schemas/fountain_pen/signatures/transformers/prose"
 
-import * as v_serialize from "astn-core/dist/implementation/manual/transformers/sealed_target/fountain_pen"
+import * as v_serialize from "astn-core/dist/implementation/manual/transformers/sealed_target/prose"
 
 import * as v_marshall from "./astn_sealed_target"
 
--- typescript/lib/src/implementation/generated/liana/schemas/get_indata/transformers/fountain_pen.ts
+++ typescript/lib/src/implementation/generated/liana/schemas/get_indata/transformers/fountain_pen.ts
@@ -3,7 +3,7 @@ import * as p_ from 'pareto-core/dist/implementation/transformer'
 
 import * as t_signatures from "../../../../../../interface/generated/liana/schemas/get_indata/signatures/transformers/fountain_pen"
 
-import * as v_serialize from "astn-core/dist/implementation/manual/transformers/sealed_target/fountain_pen"
+import * as v_serialize from "astn-core/dist/implementation/manual/transformers/sealed_target/prose"
 
 import * as v_marshall from "./astn_sealed_target"
 

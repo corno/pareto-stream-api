7451d6c643a58fe90deeaa4a89dab3e75bcf5148
Corno Schraverus <corno@schraverus.com>
2026-07-08 07:31:15 +0200

    pdt: ...

--- typescript/lib/src/interface/actions/commands.ts
+++ typescript/lib/src/interface/actions/commands.ts
@@ -1,9 +1,9 @@
 import * as p_ from 'pareto-core/interface/command_action'
 
-import * as d_stream_log from "./generated/liana/schemas/log/data.js"
-import * as d_stream_log_error from "./generated/liana/schemas/log_error/data.js"
-import * as d_stream_write_to_stderr from "./generated/liana/schemas/write_to_stderr/data.js"
-import * as d_stream_write_to_stdout from "./generated/liana/schemas/write_to_stdout/data.js"
+import * as d_stream_log from "../generated/liana/schemas/log/data.js"
+import * as d_stream_log_error from "../generated/liana/schemas/log_error/data.js"
+import * as d_stream_write_to_stderr from "../generated/liana/schemas/write_to_stderr/data.js"
+import * as d_stream_write_to_stdout from "../generated/liana/schemas/write_to_stdout/data.js"
 
 export type log = p_.Command_Action<null, d_stream_log.Parameters>
 export type log_error = p_.Command_Action<null, d_stream_log_error.Parameters>

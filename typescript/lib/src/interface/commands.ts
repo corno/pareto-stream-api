07f24c4bee9de285f76525da51ae3de73690d12c
Corno Schraverus <corno@schraverus.com>
2026-07-11 19:39:53 +0200

    pdt: ...

--- typescript/lib/src/interface/commands.ts
+++ typescript/lib/src/interface/commands.ts
@@ -1,23 +1,23 @@
 import * as p_ from 'pareto-core/interface/command_interface'
 
-import type * as d_stream_log from "./schemas/log.js"
-import type * as d_stream_log_error from "./schemas/log_error.js"
-import type * as d_stream_write_to_stderr from "./schemas/write_to_stderr.js"
-import type * as d_stream_write_to_stdout from "./schemas/write_to_stdout.js"
+import type * as s_stream_log from "./schemas/log.js"
+import type * as s_stream_log_error from "./schemas/log_error.js"
+import type * as s_stream_write_to_stderr from "./schemas/write_to_stderr.js"
+import type * as s_stream_write_to_stdout from "./schemas/write_to_stdout.js"
 
 export type log = p_.Command_Interface<
     null,
-    d_stream_log.Parameters
+    s_stream_log.Parameters
 >
 export type log_error = p_.Command_Interface<
     null,
-    d_stream_log_error.Parameters
+    s_stream_log_error.Parameters
 >
 export type write_to_stderr = p_.Command_Interface<
     null,
-    d_stream_write_to_stderr.Parameters
+    s_stream_write_to_stderr.Parameters
 >
 export type write_to_stdout = p_.Command_Interface<
     null,
-    d_stream_write_to_stdout.Parameters
+    s_stream_write_to_stdout.Parameters
 >
--- typescript/lib/src/interface/queries.ts
+++ typescript/lib/src/interface/queries.ts
@@ -1,10 +1,10 @@
 import * as p_ from 'pareto-core/interface/query_interface'
 
-import type * as d_stream_get_instream_data from "./schemas/get_indata.js"
+import type * as s_stream_get_instream_data from "./schemas/get_indata.js"
 
 
 export type get_instream_data = p_.Query_Interface<
-    d_stream_get_instream_data.Result,
+    s_stream_get_instream_data.Result,
     null,
-    d_stream_get_instream_data.Parameters
+    s_stream_get_instream_data.Parameters
 >

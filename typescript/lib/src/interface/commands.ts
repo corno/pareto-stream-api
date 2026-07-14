3f85e52437397b8c79e5b7784c1a4519d0ffe18d
Corno Schraverus <corno@schraverus.com>
2026-07-14 07:55:55 +0200

    pdt: published version 0.1.31

--- typescript/lib/src/interface/commands.ts
+++ typescript/lib/src/interface/commands.ts
@@ -1,6 +1,7 @@
 import * as p_ from 'pareto-core/interface/command_interface'
 
 import type * as s_stream_log from "./schemas/log.js"
+import type * as s_stream_log_errors from "./schemas/log_error.js"
 import type * as s_stream_log_error from "./schemas/log_error.js"
 import type * as s_stream_write_to_stderr from "./schemas/write_to_stderr.js"
 import type * as s_stream_write_to_stdout from "./schemas/write_to_stdout.js"
@@ -12,6 +13,10 @@ export type log = p_.Command_Interface<
 export type log_error = p_.Command_Interface<
     null,
     s_stream_log_error.Parameters
+>
+export type log_errors = p_.Command_Interface<
+    null,
+    s_stream_log_errors.Parameters
 >
 export type write_to_stderr = p_.Command_Interface<
     null,

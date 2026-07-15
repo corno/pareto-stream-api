5e982c27cd1ee70bc05e8eb86992a6a295dbf540
Corno Schraverus <corno@schraverus.com>
2026-07-15 17:39:16 +0200

    pdt: published version 0.1.36

--- typescript/lib/src/interface/commands.ts
+++ typescript/lib/src/interface/commands.ts
@@ -1,22 +1,22 @@
 import * as p_ from 'pareto-core/interface/command_interface'
 
-import type * as s_stream_log from "./schemas/log.js"
-import type * as s_stream_log_errors from "./schemas/log_errors.js"
-import type * as s_stream_log_error from "./schemas/log_error.js"
+import type * as s_stream_log_lines from "./schemas/log_lines.js"
+import type * as s_stream_log_error_lines from "./schemas/log_error_lines.js"
+import type * as s_stream_log_error_line from "./schemas/log_error_line.js"
 import type * as s_stream_write_to_stderr from "./schemas/write_to_stderr.js"
 import type * as s_stream_write_to_stdout from "./schemas/write_to_stdout.js"
 
-export type log = p_.Command_Interface<
+export type log_lines = p_.Command_Interface<
     null,
-    s_stream_log.Parameters
+    s_stream_log_lines.Parameters
 >
-export type log_error = p_.Command_Interface<
+export type log_error_line = p_.Command_Interface<
     null,
-    s_stream_log_error.Parameters
+    s_stream_log_error_line.Parameters
 >
-export type log_errors = p_.Command_Interface<
+export type log_error_lines = p_.Command_Interface<
     null,
-    s_stream_log_errors.Parameters
+    s_stream_log_error_lines.Parameters
 >
 export type write_to_stderr = p_.Command_Interface<
     null,
